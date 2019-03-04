997c1ed29b3ab6eedcfb489bfe2f9d704f9ed457
Sarah Nicholson <sarah.nicholson.13@ucl.ac.uk>
2019-03-04 09:47:31 +0000

    Minor change to instruction text

--- login/index.php
+++ login/index.php
@@ -13,18 +13,18 @@ require_once('initialise.php');
 <?php include("navbar.php"); ?>
 <main role="main" class="container">
     <form class="form" action="login.php" method="post">
+        <br>
         <div class="form-group">
             <label for="email">Email address:</label>
             <input type="email" class="form-control" id="email" name="email" required><br>
             <label for="password">Password:</label>
             <input type="password" class="form-control" id="password" name="password" required><br>
-            <label><input class="checkbox" type="checkbox" name="rme"> Remember me</label><br>
         </div>
         <button type="submit" class="btn btn-dark">Submit</button>
     </form>
     <hr>
     <form action="adduser.php" method="post">
-        <input type="submit" name="someAction" value="Create a test account" />
+        <input type="submit" name="addUser" value="Create a test account" />
     </form>
 </main>
 </body>
